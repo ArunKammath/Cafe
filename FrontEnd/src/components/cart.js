60f81925df2264d1e73f1390886c60bc1c8b191d
ArunVKammath <arunvenugopalkammath97@gmail.com>
2025-12-05 01:54:58 +0530

    placing order in cart

--- FrontEnd/src/components/cart.js
+++ FrontEnd/src/components/cart.js
@@ -1,8 +1,23 @@
 import "../style/cart.css";
 function Cart({items}) {
+
     const itemCount = items.itemCount;
     const itemPrice = items.itemPrice;
+    const setItemCount = items.setItemCount;
     let amount=0.00;
+    const handlePlaceOrder = () => {
+        console.log("Placed Order");
+        setItemCount({
+            tea: 0,
+            coffee: 0,
+            elanji: 0,
+            kaypola: 0,
+            ullivada: 0,
+            tenderCoconut: 0,
+            bananaHalwa: 0,
+            total: 0
+        });
+    }
     if(itemCount.total > 0) {
         let itemList = [];
         for(let item in itemCount) {
@@ -25,6 +40,7 @@ function Cart({items}) {
                         <h2>{item!=="total" ? (Number(itemPrice[item])*Number(itemCount[item])).toFixed(2) : amount.toFixed(2)}</h2>
                     </section>
                 ))}
+                <button id="placeOrder" onClick={handlePlaceOrder}>Place Order</button>
             </section>
         )
     }
